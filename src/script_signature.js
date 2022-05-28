45322edabc102a5fdbd2dfa7b08e6231da236527
Sexual <Sexual@users.noreply.github.com>
2022-05-28 21:21:27 +0100

    Update script_signature.js

--- src/script_signature.js
+++ src/script_signature.js
@@ -40,7 +40,7 @@ function encode(signature, hashType) {
     },
     { signature, hashType },
   );
-  const hashTypeMod = hashType & ~0x80;
+  const hashTypeMod = hashType & ~0xc0;
   if (hashTypeMod <= 0 || hashTypeMod >= 4)
     throw new Error('Invalid hashType ' + hashType);
   const hashTypeBuffer = Buffer.allocUnsafe(1);
